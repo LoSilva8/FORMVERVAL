80f7e5ba19bdac32fe7983a59c1a45e04b56acba
Lorenzo Silva Bonet <lorenzosilvabonet@gmail.com>
2024-10-31 11:39:56 -0300

    Update TestCalcularAreaRetangulo.py

--- TestCalcularAreaRetangulo.py
+++ TestCalcularAreaRetangulo.py
@@ -1,8 +1,14 @@
 import unittest
 
+def calcula_area_retangulo(altura, largura):
+    return altura * largura
+
 class TestCalculaAreaRetangulo(unittest.TestCase):
     def test_area_retangulo(self):
         altura = 5
         largura = 10
         esperado = 50
         self.assertEqual(calcula_area_retangulo(altura, largura), esperado)
+
+if __name__ == "__main__":
+    unittest.main()
